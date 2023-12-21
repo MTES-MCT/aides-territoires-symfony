5c223b4c408cc739fe6e44934aeec17454ff31f6
Remi BARRET <barret.remi.pro@gmail.com>
2023-12-21 15:50:15 +0100

    script sh

--- bin/start-app.sh
+++ bin/start-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo $JWT_PRIVATE_PEM | base64 -d > ./private.pem
-echo $JWT_PUBLIC_PEM | base64 -d > ./public.pem
+#echo $JWT_PUBLIC_PEM | base64 -d > ./public.pem
 # Start default script for PHP apps
-# $HOME/bin/run
+$HOME/bin/run
