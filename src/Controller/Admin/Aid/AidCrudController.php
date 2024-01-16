7c6877a4cc8b355bcaf920664f78bd7f87b578f9
Remi BARRET <barret.remi.pro@gmail.com>
2024-01-16 18:33:53 +0100

    test surcharge

--- src/Controller/Admin/Aid/AidCrudController.php
+++ src/Controller/Admin/Aid/AidCrudController.php
@@ -43,6 +43,15 @@ class AidCrudController extends AtCrudController
         return Aid::class;
     }
 
+    /**
+     * Generates a URL from the given parameters.
+     *
+     * @see UrlGeneratorInterface
+     */
+    protected function generateUrl(string $route, array $parameters = [], int $referenceType = UrlGeneratorInterface::ABSOLUTE_URL): string
+    {
+        return $this->container->get('router')->generate($route, $parameters, $referenceType);
+    }
 
     public function configureFilters(Filters $filters): Filters
     {
