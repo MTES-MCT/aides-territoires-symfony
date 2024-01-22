9e24ee5494145432983e33eeea98e032eac1de9a
Remi BARRET <barret.remi.pro@gmail.com>
2024-01-22 15:45:50 +0100

    js pour inscription

--- assets/js/front/user/register.js
+++ assets/js/front/user/register.js
@@ -3,3 +3,31 @@ require('./toggle_acquisition_channel_related_field.js');
 require('./toggle_beneficiary_related_fields.js');
 require('./toggle_intercommunality_type_field.js');
 
+$(document).ready(function () {
+    $(document).on({
+        change: function (e) {
+            completeOrganizationName();
+        }
+    }, '#register_organizationType');
+
+    $(document).on({
+        change: function (e) {
+            completeOrganizationName();
+        }
+    }, '#register_perimeter');
+    
+    
+});
+
+function completeOrganizationName()
+{
+    var perimeterTxt = $('#register_perimeter').parents().find('div.item:first').text();
+    var organizationType = $('option:selected', '#register_organizationType').text();
+
+    if (organizationType == 'Commune' && perimeterTxt != '') {
+        var organizationName = 'Marie de ' + perimeterTxt.replace(/ *\([^)]*\) */g, " ");;
+    } else {
+        organizationName = '';
+    }
+    $('#register_organizationName').val(organizationName);
+}
