ad3d5179a534e6ed4129ac38e85861a2621b34d4
Remi BARRET <barret.remi.pro@gmail.com>
2024-02-29 11:16:29 +0100

    redirection portails

---
+++ migrations/Version20240229101547.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240229101547 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX slug_search_page ON search_page (slug)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX slug_search_page ON search_page');
+    }
+}
--- src/EventListener/RouteListener.php
+++ src/EventListener/RouteListener.php
@@ -3,16 +3,29 @@
 namespace App\EventListener;
 
 use App\Controller\Page\PageController;
+use App\Controller\Portal\PortalController;
 use App\Entity\Page\Page;
+use App\Entity\Search\SearchPage;
 use App\Repository\Page\PageRepository;
+use App\Repository\Search\SearchPageRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 final class RouteListener
 {
     public function __construct(
         private EntityManagerInterface $entityManagerInterface,
-        private PageRepository $pageRepository
+        private PageRepository $pageRepository,
+        private SearchPageRepository $searchPageRepository,
+        private RouterInterface $routerInterface,
+        private KernelInterface $kernelInterface
     )
     {
         
@@ -21,10 +34,44 @@ final class RouteListener
         RequestEvent $event
     ): void
     {
+        // Si ce n'est pas la requête principale, ne fait rien
+        if (!$event->isMainRequest()) {
+            return;
+        }
+
+        //----------------------------------------------------------------------------------
+        // Si sous domaine, on va regarder si cela corresponds à une SearchPage (portail)
+        $host = $event->getRequest()->getHost();
+
+        // Sépare le nom de l'hôte en ses composants
+        $hostParts = explode('.', $host);
+
+        // Le sous-domaine est le premier composant
+        $subdomain = $hostParts[0] ?? null;
+            if ($subdomain) {
+            // on regarde si cela corresponds à un portail
+            $searchPage = $this->searchPageRepository->findOneBy(
+                [
+                    'slug' => $subdomain,
+                ]
+            );
+            if ($searchPage instanceof SearchPage) {
+                try {
+                    $url = $this->routerInterface->generate('app_portal_portal_details', ['slug' => $subdomain], UrlGeneratorInterface::ABSOLUTE_URL);
+                    $response = new RedirectResponse($url);
+                    $event->setResponse($response);
+                } catch (\Exception $e) {
+
+                }
+            }
+        }
+
+        //----------------------------------------------------------------------------------
+        // regarde si une page corresponds à l'url demandée
+        
         // url demandée
         $url = urldecode($event->getRequest()->getRequestUri()) ?? null;
-
-        // regarde si une page corresponds
+        
         $page = $this->pageRepository->findOneBy(
             [
                 'url' => $url,
@@ -40,7 +87,6 @@ final class RouteListener
                     '_controller',
                     PageController::class . '::index'
                 );
-                ;
             } catch (\Exception $e) {
 
             }
