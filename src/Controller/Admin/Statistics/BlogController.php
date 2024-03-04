c46b83b3bf6a76c5d840afd54e76ddf30e196a56
Remi BARRET <barret.remi.pro@gmail.com>
2024-03-04 12:32:04 +0100

    stats blog

--- src/Controller/Admin/Statistics/BlogController.php
+++ src/Controller/Admin/Statistics/BlogController.php
@@ -3,15 +3,25 @@
 namespace App\Controller\Admin\Statistics;
 
 use App\Controller\Admin\DashboardController;
+use App\Entity\Blog\BlogPost;
 use App\Form\Admin\Filter\DateRangeType;
+use App\Repository\Log\LogBlogPostViewRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class BlogController extends DashboardController
 {
     #[Route('/admin/statistics/blog/dashboard', name: 'admin_statistics_blog_dashboard')]
     public function blogDashboard(
-        AdminContext $adminContext
+        AdminContext $adminContext,
+        LogBlogPostViewRepository $logBlogPostViewRepository,
+        FormFactoryInterface $formFactoryInterface,
+        ChartBuilderInterface $chartBuilderInterface
     )
     {
         // dates par défaut
@@ -30,11 +40,176 @@ class BlogController extends DashboardController
             $formDateRange->get('dateMin')->setData($dateMin);
             $formDateRange->get('dateMax')->setData($dateMax);
         }
+
+        // top articles
+        $topBlogPosts = $logBlogPostViewRepository->findTopOfDateRange([
+            'dateMin' => $dateMin,
+            'dateMax' => $dateMax,
+        ]);
+
+        // top categories
+        $topBlogPostsCategories = $logBlogPostViewRepository->findTopCategoriesOfDateRange([
+            'dateMin' => $dateMin,
+            'dateMax' => $dateMax,
+        ]);
+
+        $chartTopCategories = $chartBuilderInterface->createChart(Chart::TYPE_PIE);
+
+        // première boucle pour faire les pourcentages
+        $total = 0;
+        foreach ($topBlogPostsCategories as $category) {
+            $total += $category['nb'];
+        }
+        foreach ($topBlogPostsCategories as $key => $category) {
+            $topBlogPostsCategories[$key]['percentage'] = $total == 0 ? 0 : number_format(($category['nb'] * 100 / $total), 2);
+        }
+
+        $labels = [];
+        $datas = [];
+        $categories = [];
+        foreach ($topBlogPostsCategories as $category) {
+            $labels[] = $category['blogPostCategory']->getName(). ' ('.$category['percentage'].'%)';
+            $datas[] = $category['nb'];
+            $categories[] = $category['blogPostCategory'];
+        }
+        $colors = $this->getCategoriesBackgroundColor($categories);
+
+        $chartTopCategories->setData([
+            'labels' => $labels,
+            'datasets' => [
+                [
+                    'label' => 'Nombre de vues',
+                    'backgroundColor' => $colors,
+                    'data' => $datas,
+                ],
+            ],
+        ]);
+        
+
+        // formulaire de filtre evolution
+        // dates par défaut
+        $dateMinEvolution = new \DateTime('-1 month');
+        $dateMaxEvolution = new \DateTime();
+        
+
+        $formDateRangeEvolution = $formFactoryInterface->createNamed('date_range_evolution', DateRangeType::class, null, [
+            'action' => $this->adminUrlGenerator->generateUrl('admin_statistics_blog_dashboard').'#evolution',
+        ]);
+        $formDateRangeEvolution->add('blogPost', EntityType::class, [
+            'required' => true,
+            'class' => BlogPost::class,
+            'choice_label' => 'name',
+            'label' => 'Article',
+            'placeholder' => 'Sélectionnez un article',
+            'query_builder' => function ($er) {
+                return $er->createQueryBuilder('bp')
+                    ->orderBy('bp.datePublished', 'DESC');
+            },
+            'autocomplete' => true,
+        ]);
+        $formDateRangeEvolution->handleRequest($adminContext->getRequest());
+        if ($formDateRangeEvolution->isSubmitted()) {
+            if ($formDateRangeEvolution->isValid()) {
+                $dateMinEvolution = $formDateRangeEvolution->get('dateMin')->getData();
+                $dateMaxEvolution = $formDateRangeEvolution->get('dateMax')->getData();
+
+                // les vues
+                $views = $logBlogPostViewRepository->countByDate([
+                    'dateMin' => $dateMinEvolution,
+                    'dateMax' => $dateMaxEvolution,
+                    'blogPost' => $formDateRangeEvolution->get('blogPost')->getData(), 
+                ]);
+                
+                // on transforme en tableau par dates
+                $viewByDate = [];
+                foreach ($views as $view) {
+                    $viewByDate[$view['dateCreate']->format('Y-m-d')] = [
+                        'date' => $view['dateCreate']->format('Y-m-d'),
+                        'nb' => $view['nb']
+                    ];
+                }
+                
+                // on refait le tableau en prenant toutes les dates
+                $viewsByDateFinal = [];
+                $dateStart = clone $dateMinEvolution;
+                while ($dateStart <= $dateMaxEvolution) {
+                    $date = $dateStart->format('Y-m-d');
+                    $viewsByDateFinal[$date] = [
+                        'date' => $date,
+                        'nb' => $viewByDate[$date]['nb'] ?? 0
+                    ];
+                    $dateStart->modify('+1 day');
+                }
+                
+                $chartEvolution = $chartBuilderInterface->createChart(Chart::TYPE_LINE);
+
+                $labels = [];
+                $datas = [];
+                foreach ($viewsByDateFinal as $view) {
+                    $labels[] = $view['date'];
+                    $datas[] = $view['nb'];
+                }
+                $chartEvolution->setData([
+                    'labels' => $labels,
+                    'datasets' => [
+                        [
+                            'label' => 'Nombre de vues',
+                            'backgroundColor' => 'rgb(255, 99, 132)',
+                            'borderColor' => 'rgb(255, 99, 132)',
+                            'data' => $datas,
+                        ],
+                    ],
+                ]);
+        
+                $chartEvolution->setOptions([
+                    'scales' => [
+                        'y' => [
+                            'beginAtZero' => true,
+                        ],
+                    ],
+                ]);
+
+            }
+        } else {
+            $formDateRangeEvolution->get('dateMin')->setData($dateMinEvolution);
+            $formDateRangeEvolution->get('dateMax')->setData($dateMaxEvolution);
+        }
+        
         
         return $this->render('admin/statistics/blog/dashboard.html.twig', [
             'formDateRange' => $formDateRange,
             'dateMin' => $dateMin,
             'dateMax' => $dateMax,
+            'topBlogPosts' => $topBlogPosts,
+            'chartTopCategories' => $chartTopCategories,
+            'formDateRangeEvolution' => $formDateRangeEvolution,
+            'dateMinEvolution' => $dateMinEvolution,
+            'dateMaxEvolution' => $dateMaxEvolution,
+            'chartEvolution' => $chartEvolution ?? null,
         ]);
+    }
+
+    private function getCategoriesBackgroundColor(array $array) {
+        $colorsBySlug = [
+            'webinaires' => 'rgb(255, 99, 132)',
+            'article' => 'rgb(54, 162, 235)',
+            'outre-mer' => 'rgb(255, 205, 86)',
+            'mobilite' => 'rgb(75, 192, 192)',
+            'nouvelle-fonctionnalite' => 'rgb(153, 102, 255)',
+            'fonds-vert' => 'rgb(255, 159, 64)',
+            'europe' => 'rgb(201, 203, 207)',
+            'default' => 'rgb(75, 192, 192)',
+        ];
+        $returnColors = [];
+
+        foreach ($array as $blogPostCategory) {
+            if (!isset($colorsBySlug[$blogPostCategory->getSlug()])) {
+                $returnColors[] = $colorsBySlug['default'];
+            } else {
+                $returnColors[] = $colorsBySlug[$blogPostCategory->getSlug()];
+            }
+        }
+
+        return $returnColors;
     }
 }
--- src/Repository/Log/LogBlogPostViewRepository.php
+++ src/Repository/Log/LogBlogPostViewRepository.php
@@ -2,8 +2,12 @@
 
 namespace App\Repository\Log;
 
+use App\Entity\Blog\BlogPost;
+use App\Entity\Blog\BlogPostCategory;
 use App\Entity\Log\LogBlogPostView;
+use App\Entity\Organization\Organization;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -21,28 +25,107 @@ class LogBlogPostViewRepository extends ServiceEntityRepository
         parent::__construct($registry, LogBlogPostView::class);
     }
 
-//    /**
-//     * @return LogBlogPostView[] Returns an array of LogBlogPostView objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('l.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    public function countByDate(?array $params = null): array
+    {
+        $qb = $this->getQueryBuilder($params);
+        $qb
+        ->select('lbpv.dateCreate, COUNT(lbpv.id) AS nb')
+        ->groupBy('lbpv.dateCreate')
+        ;
+        return $qb->getQuery()->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?LogBlogPostView
-//    {
-//        return $this->createQueryBuilder('l')
-//            ->andWhere('l.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    public function findTopCategoriesOfDateRange(?array $params = null): array
+    {
+        $params['maxResults'] = $params['maxResults'] ?? 10;
+        $qb = $this->getQueryBuilder($params);
+
+        $qb
+            ->innerJoin('lbpv.blogPost', 'blogPostCount')
+            ->innerJoin('blogPostCount.blogPostCategory', 'blogPostCategoryCount')
+            ->select('blogPostCategoryCount.id, COUNT(lbpv.id) AS nb')
+            ->groupBy('blogPostCategoryCount.id')
+            ->orderBy('nb', 'DESC')
+            ;
+        $results = $qb->getQuery()->getResult();
+        $return = [];
+        foreach ($results as $result) {
+            $return[] = [
+                'blogPostCategory' => $this->_em->getReference(BlogPostCategory::class, $result['id']),
+                'nb' => $result['nb']
+            ];
+        }
+        return $return;
+    }
+
+    public function findTopOfDateRange(?array $params = null): array
+    {
+        $params['maxResults'] = $params['maxResults'] ?? 10;
+        $qb = $this->getQueryBuilder($params);
+
+        $qb
+            ->innerJoin('lbpv.blogPost', 'blogPostCount')
+            ->select('blogPostCount.id, COUNT(lbpv.id) AS nb')
+            ->groupBy('blogPostCount.id')
+            ->orderBy('nb', 'DESC')
+            ;
+        $results = $qb->getQuery()->getResult();
+        $return = [];
+        foreach ($results as $result) {
+            $return[] = [
+                'blogPost' => $this->_em->getReference(BlogPost::class, $result['id']),
+                'nb' => $result['nb']
+            ];
+        }
+        return $return;
+    }
+
+    public function getQueryBuilder(array $params = null): QueryBuilder
+    {
+        $dateMin = $params['dateMin'] ?? null;
+        $dateMax = $params['dateMax'] ?? null;
+        $blogPost = $params['blogPost'] ?? null;
+        $organization = $params['organization'] ?? null;
+        $maxResults = $params['maxResults'] ?? null;
+
+        $qb = $this->createQueryBuilder('lbpv');
+
+        if ($blogPost instanceof BlogPost && $blogPost->getId()) {
+            $qb
+                ->innerJoin('lbpv.blogPost', 'blogPost')
+                ->andWhere('lbpv.blogPost = :blogPost')
+                ->setParameter('blogPost', $blogPost)
+                ;
+        }
+        
+        if ($organization instanceof Organization && $organization->getId()) {
+            $qb
+                ->innerJoin('lbpv.organization', 'organization')
+                ->andWhere('lbpv.organization = :organization')
+                ->setParameter('organization', $organization)
+                ;
+        }
+        
+        if ($dateMin instanceof \DateTime) {
+            $qb
+                ->andWhere('lbpv.dateCreate >= :dateMin')
+                ->setParameter('dateMin', $dateMin)
+            ;
+        }
+
+        if ($dateMax instanceof \DateTime) {
+            $qb
+                ->andWhere('lbpv.dateCreate <= :dateMax')
+                ->setParameter('dateMax', $dateMax)
+            ;
+        }
+
+        if ($maxResults !== null) {
+            $qb
+                ->setMaxResults($maxResults)
+            ;
+        }
+
+        return $qb;
+    }
 }
