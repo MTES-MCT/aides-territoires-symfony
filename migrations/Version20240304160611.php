762418d66e08db9bd8caad98ddf756cdc3d26626
Remi BARRET <barret.remi.pro@gmail.com>
2024-03-06 10:49:08 +0100

    change la migration

--- migrations/Version20240304160611.php
+++ migrations/Version20240304160611.php
@@ -22,6 +22,23 @@ final class Version20240304160611 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE aid_step ADD active TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE aid_type ADD active TINYINT(1) DEFAULT NULL');
+        $this->addSql('UPDATE aid_type SET active = 1');
+        $this->addSql('UPDATE aid_step SET active = 1');
+        $this->addSql("
+        INSERT INTO aid_type (aid_type_group_id,name,slug,`position`,time_create,time_update,active) VALUES
+            (2,'Ingénierie de planification et stratégie','ingenierie-de-planification-et-strategie',8,'2024-03-04 16:20:57','2024-03-04 16:20:57',1),
+            (2,'Ingénierie d’études et diagnostics','ingenierie-detudes-et-diagnostics',9,'2024-03-04 16:21:24','2024-03-04 16:21:24',1),
+            (2,'Ingénierie d’animation et mise en réseau','ingenierie-danimation-et-mise-en-reseau',10,'2024-03-04 16:21:45','2024-03-04 16:21:45',1),
+            (2,'AMOA/MOD','amoa-mod',11,'2024-03-04 16:21:58','2024-03-04 16:21:58',1),
+            (2,'MOE/MOE déléguée','moe-moe-deleguee',12,'2024-03-04 16:22:12','2024-03-04 16:22:12',1),
+            (2,'Ingénierie administrative','ingenierie-administrative',13,'2024-03-04 16:28:41','2024-03-04 16:28:41',1),
+            (2,'Ingénierie juridique et réglementaire','ingenierie-juridique-et-reglementaire',14,'2024-03-04 16:28:58','2024-03-04 16:28:58',1)
+        ");
+        $this->addSql("
+        INSERT INTO aid_step (name,slug,`position`,time_create,time_update,active) VALUES
+            ('Émergence / stratégie','emergence-stategie',3,'2024-03-04 15:06:37',NULL,1),
+            ('Conception / faisabilité','conception-faisabilite',4,'2024-03-04 15:06:37',NULL,1)
+        ");
     }
 
     public function down(Schema $schema): void
--- migrations/Version20240305082433.php
+++ migrations/Version20240305082433.php
@@ -24,21 +24,6 @@ final class Version20240305082433 extends AbstractMigration
         $this->addSql('ALTER TABLE aid ADD aid_type_support_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE aid ADD CONSTRAINT FK_48B40DAAF635BDEE FOREIGN KEY (aid_type_support_id) REFERENCES aid_type_support (id) ON DELETE SET NULL');
         $this->addSql('CREATE INDEX IDX_48B40DAAF635BDEE ON aid (aid_type_support_id)');
-        $this->addSql("
-        INSERT INTO aid_type (aid_type_group_id,name,slug,`position`,time_create,time_update,active) VALUES
-            (2,'Ingénierie de planification et stratégie','ingenierie-de-planification-et-strategie',8,'2024-03-04 16:20:57','2024-03-04 16:20:57',1),
-            (2,'Ingénierie d’études et diagnostics','ingenierie-detudes-et-diagnostics',9,'2024-03-04 16:21:24','2024-03-04 16:21:24',1),
-            (2,'Ingénierie d’animation et mise en réseau','ingenierie-danimation-et-mise-en-reseau',10,'2024-03-04 16:21:45','2024-03-04 16:21:45',1),
-            (2,'AMOA/MOD','amoa-mod',11,'2024-03-04 16:21:58','2024-03-04 16:21:58',1),
-            (2,'MOE/MOE déléguée','moe-moe-deleguee',12,'2024-03-04 16:22:12','2024-03-04 16:22:12',1),
-            (2,'Ingénierie administrative','ingenierie-administrative',13,'2024-03-04 16:28:41','2024-03-04 16:28:41',1),
-            (2,'Ingénierie juridique et réglementaire','ingenierie-juridique-et-reglementaire',14,'2024-03-04 16:28:58','2024-03-04 16:28:58',1)
-        ");
-        $this->addSql("
-        INSERT INTO aid_step (name,slug,`position`,time_create,time_update,active) VALUES
-            ('Émergence / stratégie','emergence-stategie',3,'2024-03-04 15:06:37',NULL,1),
-            ('Conception / faisabilité','conception-faisabilite',4,'2024-03-04 15:06:37',NULL,1)
-        ");
     }
 
     public function down(Schema $schema): void
