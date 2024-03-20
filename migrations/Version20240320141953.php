c92951c4d18cd693138b9a102ac1ce9157ed4bcc
Remi BARRET <barret.remi.pro@gmail.com>
2024-03-20 16:52:12 +0100

    correction migration

--- migrations/Version20240320141953.php
+++ migrations/Version20240320141953.php
@@ -20,12 +20,12 @@ final class Version20240320141953 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE backer ADD nb_aids INT DEFAULT NULL, ADD nb_aids_live INT DEFAULT NULL, DROP active');
+        $this->addSql('ALTER TABLE backer ADD nb_aids INT DEFAULT NULL, ADD nb_aids_live INT DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE backer ADD active TINYINT(1) NOT NULL, DROP nb_aids, DROP nb_aids_live');
+        $this->addSql('ALTER TABLE backer DROP nb_aids, DROP nb_aids_live');
     }
 }
