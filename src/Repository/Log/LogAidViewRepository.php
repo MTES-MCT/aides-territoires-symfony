1c3a694132b356a7adcef4996c7f27b456041c7e
Remi BARRET <barret.remi.pro@gmail.com>
2024-03-25 14:16:19 +0100

    stats export

--- src/Repository/Log/LogAidViewRepository.php
+++ src/Repository/Log/LogAidViewRepository.php
@@ -133,6 +133,7 @@ class LogAidViewRepository extends ServiceEntityRepository
     {
         $dateMin = $params['dateMin'] ?? null;
         $dateMax = $params['dateMax'] ?? null;
+        $dateCreate = $params['dateCreate'] ?? null;
         $author = $params['author'] ?? null;
         $aid = $params['aid'] ?? null;
         $aidIds = $params['aidIds'] ?? null;
@@ -168,6 +169,14 @@ class LogAidViewRepository extends ServiceEntityRepository
                 ->setParameter('aidIds', $aidIds)
             ;
         }
+
+        if ($dateCreate instanceof \DateTime) {
+            $qb
+                ->andWhere('lav.dateCreate = :dateCreate')
+                ->setParameter('dateCreate', $dateCreate)
+            ;
+        }
+        
         if ($dateMin instanceof \DateTime) {
             $qb
                 ->andWhere('lav.dateCreate >= :dateMin')
