04255ce5f8a18405d47bb0e23ff3f51ea12eec7b
Remi BARRET <barret.remi.pro@gmail.com>
2024-04-18 11:18:15 +0200

    correction migration

--- migrations/Version20240304160611.php
+++ migrations/Version20240304160611.php
@@ -34,7 +34,7 @@ final class Version20240304160611 extends AbstractMigration
             (2,'AMOA/MOD','amoa-mod',11,'2024-03-04 16:21:58','2024-03-04 16:21:58',1),
             (2,'MOE/MOE déléguée','moe-moe-deleguee',12,'2024-03-04 16:22:12','2024-03-04 16:22:12',1),
             (2,'Ingénierie administrative','ingenierie-administrative',13,'2024-03-04 16:28:41','2024-03-04 16:28:41',1),
-            (2,'Ingénierie juridique et réglementaire','ingenierie-juridique-et-reglementaire',14,'2024-03-04 16:28:58','2024-03-04 16:28:58',1)
+            (2,'Ingénierie juridique et réglementaire','ingenierie-juridique-et-reglementaire',14,'2024-03-04 16:28:58','2024-03-04 16:28:58',1),
             (2,'Formation/montée en compétence','formation-montee-en-competence',14,'2024-03-04 16:28:58','2024-03-04 16:28:58',1)
         ");
         $this->addSql("
