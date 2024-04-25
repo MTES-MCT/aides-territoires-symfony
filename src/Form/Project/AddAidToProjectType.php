86d61c49f1836b44e161b258c8eaf3756941c442
Remi BARRET <barret.remi.pro@gmail.com>
2024-04-25 13:17:08 +0200

    formulaire ajout aide à un projet

--- src/Form/Project/AddAidToProjectType.php
+++ src/Form/Project/AddAidToProjectType.php
@@ -4,6 +4,7 @@ namespace App\Form\Project;
 
 use App\Entity\Project\Project;
 use App\Entity\User\User;
+use App\Service\Organization\OrganizationService;
 use App\Service\User\UserService;
 use Doctrine\ORM\EntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
@@ -17,31 +18,53 @@ class AddAidToProjectType extends AbstractType
 {
     public function __construct(
         protected UserService $userService,
-        protected ManagerRegistry $managerRegistry
+        protected ManagerRegistry $managerRegistry,
+        protected OrganizationService $organizationService
     )
     {   
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // le user
         $user = $this->userService->getUserLogged();
+
+        // l'aide
         $currentAid = $options['currentAid'] ?? null;
+
+        // on va recuperer tous les projets de toutes les organizations ou l'utilisateur à  les droits
+        $organizations = $this->userService->getOrganizations($user);
         $organizationProjects = [];
-        if ($user instanceof User && $user->getDefaultOrganization()) {
-            $organizationProjects = $this->managerRegistry->getRepository(Project::class)->findBy(['organization' => $user->getDefaultOrganization()]);
+        $idsProject = [];
+        foreach ($organizations as $organization) {
+            if ($this->organizationService->canEditProject($user, $organization)) {
+                foreach ($organization->getProjects() as $project) {
+                    $organizationProjects[] = $project;
+                    $idsProject[] = $project->getId();
+                }
+            }
         }
 
-        if (count($organizationProjects) > 0) {
+        if (count($idsProject) > 0) {
             $builder
             ->add('projects', EntityType::class, [
                 'required' => false,
                 'label' => false,
                 'class' => Project::class,
-                'choice_label' => 'name',
-                'query_builder' => function (EntityRepository $er) use ($user) {
+                'choice_label' => function (Project $project) {
+                    $return = $project->getName();
+                    if ($project->getOrganization()) {
+                        $return .= ' ('.$project->getOrganization()->getName().')';
+                    }
+                    return $return;
+                },
+                'query_builder' => function (EntityRepository $er) use ($idsProject) {
                     return $er->createQueryBuilder('p')
-                    ->andWhere('p.organization = :organization')
-                    ->setParameter('organization', $user->getDefaultOrganization())
+                    ->andWhere('p.id IN (:idsProject)')
+                    ->setParameter('idsProject', $idsProject)
+                    ->leftJoin('p.organization', 'organization')
+                    ->orderBy('organization.name', 'ASC')
+                    ->addOrderBy('p.name', 'ASC')
                     ;
                 },
                 'choice_attr' => function($project) use ($currentAid) {
