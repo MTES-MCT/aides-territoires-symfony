7248b65c1e4ecb11f336e9fd03f826dcd39f9b3b
Remi BARRET <barret.remi.pro@gmail.com>
2024-04-26 17:30:11 +0200

    amelioration securité du js

--- assets/js/front/organization/collaborateurs.js
+++ assets/js/front/organization/collaborateurs.js
@@ -2,12 +2,6 @@
 $(function(){
     $(document).on({
         click: function (e) {
-            $('#btn-confirm-exclude').attr('href', $(this).attr('data-url'));
-        }
-    }, '.exclude-collaborators-modal-btn');
-
-    $(document).on({
-        click: function (e) {
             $(this).parents('tr:first').remove();
         }
     }, '.btn-delete-collaborator');
