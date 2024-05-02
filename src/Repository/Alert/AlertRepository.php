47dbc95e2cac6b901efe22f4150a21fb8308779c
Remi BARRET <barret.remi.pro@gmail.com>
2024-05-02 14:30:40 +0200

    a tester

--- src/Repository/Alert/AlertRepository.php
+++ src/Repository/Alert/AlertRepository.php
@@ -47,11 +47,11 @@ class AlertRepository extends ServiceEntityRepository
     public function findToSendDaily(array $params = null) : array {
         $yesterday = new \DateTime(date('Y-m-d', strtotime('-1 day')));
 
-        $params['dateLatestAlertMin'] = $yesterday;
+        // $params['dateLatestAlertMin'] = $yesterday;
         $params['hasQueryString'] = true;
         $params['alertFrequency'] = Alert::FREQUENCY_DAILY_SLUG;
         $qb = $this->getQueryBuilder($params);
-
+dd($qb->getQuery()->getSql());
         return $qb->getQuery()->getResult();
     }
 
