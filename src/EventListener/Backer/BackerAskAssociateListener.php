d91899b164ed93c84690a923d3598628616b737c
Remi BARRET <barret.remi.pro@gmail.com>
2024-05-22 17:09:27 +0200

    modifs demandées

--- src/EventListener/Backer/BackerAskAssociateListener.php
+++ src/EventListener/Backer/BackerAskAssociateListener.php
@@ -3,10 +3,20 @@
 namespace App\EventListener\Backer;
 
 use App\Entity\Backer\BackerAskAssociate;
+use App\Service\Notification\NotificationService;
 use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 class BackerAskAssociateListener
 {
+    public function __construct(
+        private NotificationService $notificationService,
+        private RouterInterface $routerInterface
+    )
+    {
+    }
+
     public function onPostUpdate(PostUpdateEventArgs $args): void {
         /** @var BackerAskAssociate $entity */
         $entity = $args->getObject();
@@ -23,6 +33,20 @@ class BackerAskAssociateListener
                 // sauvegarde
                 $manager->persist($entity->getBacker());
                 $manager->flush();
+
+                // url fiche backer
+                $backerUrl = $this->routerInterface->generate('app_backer_details', ['id' => $entity->getBacker()->getId(), 'slug' => $entity->getBacker()->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+                // envoi de la notification à tous les utilisateurs de la structure
+                foreach ($entity->getBacker()->getOrganizations() as $organization) {
+                    foreach ($organization->getBeneficiairies() as $beneficiairy) {
+                        $this->notificationService->addNotification(
+                            $beneficiairy,
+                            'Association de la structure '.$organization->getName(). ' avec le porteur d\'aides '.$entity->getBacker()->getName(),
+                            'La fiche du porteur d\'aides '.$entity->getBacker()->getName().' a été associée avec la structure '.$organization->getName().'. Vous pouvez la consulter en cliquant sur <a href="'.$backerUrl.'" title="Voir la fiche du porteur d\'aides">ce lien</a>.',
+                        );
+                    }
+                }
             }
         }
     }
