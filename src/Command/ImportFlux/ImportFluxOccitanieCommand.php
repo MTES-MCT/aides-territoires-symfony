a3b6f2d234a14feeb71ca2df4a62686ba18cac87
Remi BARRET <barret.remi.pro@gmail.com>
2024-06-18 14:43:29 +0200

    flux occitanie

--- src/Command/ImportFlux/ImportFluxOccitanieCommand.php
+++ src/Command/ImportFlux/ImportFluxOccitanieCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use App\Command\ImportFlux\ImportFluxCommand;
 use App\Entity\Aid\Aid;
 use App\Entity\Category\Category;
+use App\Entity\Category\CategoryTheme;
 
 #[AsCommand(name: 'at:import_flux:occitanie', description: 'Import de flux Occitanie')]
 class ImportFluxOccitanieCommand extends ImportFluxCommand
@@ -38,55 +39,119 @@ class ImportFluxOccitanieCommand extends ImportFluxCommand
         } catch (\Exception $e) {
             $timeUpdate = null;
         }
-        try {
-            $timeCreate = new \DateTime($aidToImport['record_timestamp'] ?? null);
-        } catch (\Exception $e) {
-            $timeCreate = null;
-        }
 
         $description = '';
         if (isset($aidToImport['fields']['chapo'])) {
-            $description .= $this->htmlSanitizerInterface->sanitize($aidToImport['fields']['chapo']);
+            $description .= $this->getCleanHtml($aidToImport['fields']['chapo']);
         }
-        if (isset($aidToImport['fields']['description'])) {
-            $description .= $this->htmlSanitizerInterface->sanitize($aidToImport['fields']['description']);
-        }
-        if (trim($description) == '') {
-            $description = null;
+        if (isset($aidToImport['fields']['introduction'])) {
+            $description .= $this->getCleanHtml($aidToImport['fields']['introduction']);
         }
 
         return [
-            'name' => isset($aidToImport['fields']['titre']) ? strip_tags($aidToImport['fields']['titre']) : null,
-            'nameInitial' => isset($aidToImport['fields']['titre']) ? strip_tags($aidToImport['fields']['titre']) : null,
+            'name' => isset($aidToImport['fields']['titre']) ? html_entity_decode(strip_tags($aidToImport['fields']['titre']), ENT_QUOTES, 'UTF-8') : null,
+            'nameInitial' => isset($aidToImport['fields']['titre']) ? html_entity_decode(strip_tags($aidToImport['fields']['titre']), ENT_QUOTES, 'UTF-8') : null,
             'timePublished' => $timePublished,
-            'description' => $description,
             'timeUpdate'=> $timeUpdate,
+            'description' => $description,
             'originUrl' => $aidToImport['fields']['url'] ?? null,
-            'timeCreate' => $timeCreate,
             'importRawObjectCalendar' => null,
             'isCallForProject' => (isset($aidToImport['fields']['type']) && $aidToImport['fields']['type'] == 'Appels à projets') ? true : false,
-            'contact' => "Pour contacter la Région Occitanie ou candidater à l'offre, veuillez cliquer sur le bouton 'Plus d'informations' ou sur le bouton 'Candidater à l'aide'.",
             'importDataMention' => 'Ces données sont mises à disposition par la Région Occitanie.',
         ];
     }
 
     protected function setCategories(array $aidToImport, Aid $aid): Aid
     {
-        $categories = (isset($aidToImport['fields']) && isset($aidToImport['fields']['thematiques']))
+        $categories = (isset($aidToImport['fields']['thematiques']) && $aidToImport['fields']['thematiques'])
                     ? explode(',', $aidToImport['fields']['thematiques'])
                     : [];
 
-        foreach ($categories as $category) {
-            try {
-                $category = $this->managerRegistry->getRepository(Category::class)->findOneBy(['name' => $category]);
-                if (!$category instanceof Category) {
-                    throw new \Exception('Impossible de charger la catégorie : ' . $category);
+        // une partie du mapping à été fait sur les CategoryTheme et non sur les catégories
+        $mappingThemes = [
+            'Culture' => [
+                'culture-patrimoine-sports-tourisme',
+            ],
+            'Environnement - Climat' => [
+                'nature-environnement-risques'
+            ],
+            'Solidarités - Santé - Égalités' => [
+                'solidarites-lien-social'
+            ],
+            'Aménagement du territoire' => [
+                'urbanisme-logement-amenagement'
+            ],
+            'Économie et vie des entreprises' => [
+                'developpement-economique'
+            ],
+            'Transport et mobilité' => [
+                'mobilite-transports'
+            ]
+        ];
+
+        // l'autre partie est bien sur les catégories
+        $mapping = [
+            'Citoyenneté et démocratie' => [
+                'citoyennete'
+            ],
+            'Enseignement supérieur - Recherche' => [
+                'education',
+                'innovation-et-recherche'
+            ],
+            'Europe et international' => [
+                'cooperation-transfrontaliere'
+            ],
+            'Agriculture et alimentation' => [
+                'agriculture'
+            ],
+            'Formation - Orientation - Éducation' => [
+                'education'
+            ],
+            'Sport' => [
+                'sport'
+            ],
+            'Mer' => [
+                'mers'
+            ],
+            'Montagne' => [
+                'montagne'
+            ],
+            'Tourisme' => [
+                'tourisme'
+            ],
+            'Sûreté - Sécurité' => [
+                'securite'
+            ]
+            
+        ];
+        
+        foreach ($categories as $categoryName) {
+            $categoryName = trim($categoryName);
+            if (isset($mapping[$categoryName])) {
+                foreach ($mapping[$categoryName] as $slugCategory) {
+                    $category = $this->managerRegistry->getRepository(Category::class)->findOneBy([
+                        'slug' => $slugCategory
+                    ]);
+                    if ($category instanceof Category) {
+                        $aid->addCategory($category);
+                    }
                 }
-                $aid->addCategory($category);
-            } catch (\Exception $e) {
-                continue;
+            }
+
+            if (isset($mappingThemes[$categoryName])) {
+                foreach ($mappingThemes[$categoryName] as $slugCategoryTheme) {
+                    $categoryTheme = $this->managerRegistry->getRepository(CategoryTheme::class)->findOneBy([
+                        'slug' => $slugCategoryTheme
+                    ]);
+                    if ($categoryTheme instanceof CategoryTheme) {
+                        foreach ($categoryTheme->getCategories() as $category) {
+                            $aid->addCategory($category);
+                        }
+                    }
+                }
             }
         }
+
         return $aid;
     }
 }
