3d96c1f7326af3be7037de8dc7f59e0ca2d0596e
Remi BARRET <barret.remi.pro@gmail.com>
2024-07-11 14:02:03 +0200

    textes

--- src/MessageHandler/Perimeter/MsgPerimeterImportHandler.php
+++ src/MessageHandler/Perimeter/MsgPerimeterImportHandler.php
@@ -16,44 +16,7 @@ use Symfony\Component\Process\Process;
 class MsgPerimeterImportHandler
 {
     public function __construct(
-        private ManagerRegistry $managerR<?php
-
-        namespace App\MessageHandler\Perimeter;
-        
-        use App\Entity\Backer\Backer;
-        use App\Entity\Perimeter\Perimeter;
-        use App\Message\Perimeter\CountyCountBacker;
-        use Symfony\Component\Messenger\Attribute\AsMessageHandler;
-        use Doctrine\Persistence\ManagerRegistry;
-        
-        #[AsMessageHandler()]
-        class CountyCountBackerHandler
-        {
-            public function __construct(
-                private ManagerRegistry $managerRegistry,
-            ) {
-            }
-        
-            public function __invoke(CountyCountBacker $message): void
-            {
-                /** @var PerimeterRepository $perimeterRepo */
-                $perimeterRepo = $this->managerRegistry->getRepository(Perimeter::class);
-        
-                /** @var BackerRepository $backerRepo */
-                $backerRepo = $this->managerRegistry->getRepository(Backer::class);
-                
-                // charge le département
-                $county = $perimeterRepo->find($message->getIdPerimeter());
-        
-                // met à jour le nombre de porteurs
-                if ($county instanceof Perimeter) {
-                    $county->setBackersCount($backerRepo->countBackerWithAidInCounty(['id' => $county->getId()]));
-                    $this->managerRegistry->getManager()->persist($county);
-                    $this->managerRegistry->getManager()->flush();
-                }
-            }
-        }
-        egistry,
+        private ManagerRegistry $managerRegistry,
         private KernelInterface $kernelInterface,
         private NotificationService $notificationService,
         private ParamService $paramService
