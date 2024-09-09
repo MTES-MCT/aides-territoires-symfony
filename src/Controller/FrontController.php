5a7175ae6e6248f0fb94596b1858606c536c5ac2
Remi BARRET <barret.remi.pro@gmail.com>
2024-09-09 10:00:29 +0200

    retire debug

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -34,10 +34,4 @@ class FrontController extends AbstractController
             $this->translatorInterface->trans($message)
         );
     }
-
-    #[Route('/admin/test-conf', name: 'app_test_conf')]
-    public function test()
-    {
-        dd($_SERVER);
-    }
 }
