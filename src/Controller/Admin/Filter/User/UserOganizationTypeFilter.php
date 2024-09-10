2316043903a1e1e15de348deb2dd50b22e390588
Remi BARRET <barret.remi.pro@gmail.com>
2024-09-10 16:18:12 +0200

    ajoute filtre sur type de structure

---
+++ src/Controller/Admin/Filter/User/UserOganizationTypeFilter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller\Admin\Filter\User;
+
+use App\Form\Admin\Filter\User\UserOganizationTypeFilterType;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Contracts\Filter\FilterInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\FilterDataDto;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\FilterTrait;
+
+class UserOganizationTypeFilter implements FilterInterface
+{
+    use FilterTrait;
+
+    public static function new(string $propertyName, $label = null): self
+    {
+        return (new self())
+            ->setFilterFqcn(__CLASS__)
+            ->setProperty($propertyName)
+            ->setLabel($label)
+            ->setFormType(UserOganizationTypeFilterType::class);
+    }
+
+    public function apply(QueryBuilder $queryBuilder, FilterDataDto $filterDataDto, ?FieldDto $fieldDto, EntityDto $entityDto): void
+    {
+        if (!$filterDataDto->getValue()) {
+            return;
+        }
+
+        $queryBuilder
+            ->innerJoin('entity.organizations', 'organizations')
+            ->innerJoin('organizations.organizationType', 'organizationType')
+            ->andWhere('organizationType.id = :id')
+            ->setParameter('id', $filterDataDto->getValue());
+    ;
+
+
+        return;
+    }
+}
