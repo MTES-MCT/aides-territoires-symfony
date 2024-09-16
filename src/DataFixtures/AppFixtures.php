a324c59a716f28cddbcf66a872d46f05a1f081b3
Remi BARRET <barret.remi.pro@gmail.com>
2024-09-16 09:25:53 +0200

    ajout datas aux fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,10 +2,21 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Aid\Aid;
+use App\Entity\Aid\AidDestination;
+use App\Entity\Aid\AidFinancer;
+use App\Entity\Aid\AidRecurrence;
+use App\Entity\Aid\AidStep;
+use App\Entity\Aid\AidType;
+use App\Entity\Backer\Backer;
+use App\Entity\Category\Category;
+use App\Entity\Category\CategoryTheme;
 use App\Entity\Organization\Organization;
 use App\Entity\Organization\OrganizationType;
 use App\Entity\Organization\OrganizationTypeGroup;
 use App\Entity\Perimeter\Perimeter;
+use App\Entity\Program\Program;
+use App\Entity\Reference\ProjectReference;
 use App\Entity\User\User;
 use App\Service\Various\StringService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -33,10 +44,13 @@ class AppFixtures extends Fixture
     ) {
         // Supprimer et recréer la base de données
         $this->resetDatabase();
+        // $this->updateSchema($managerRegistry->getManager());
     }
 
     public function load(ObjectManager $manager): void
     {
+
+        
         // perimeterTest
         $perimeter = new Perimeter();
         $perimeter->setName('perimeterTest');
@@ -76,6 +90,106 @@ class AppFixtures extends Fixture
         $admin->addRole(User::ROLE_ADMIN);
         $admin->setTotpSecret('rand0mT0tpS3cr3t');
         $manager->persist($admin);
+
+        // Backer
+        $backer = new Backer();
+        $backer->setName('Backer test');
+        $backer->setSlug($this->stringService->getSlug($backer->getName()));
+        $backer->setPerimeter($perimeter);
+        $backer->addOrganization($organization);
+        $backer->setActive(true);
+        $backer->setIsCorporate(false);
+        $backer->setIsSpotlighted(false);
+        $manager->persist($backer);
+
+        // Program
+        $program = new Program();
+        $program->setName('Program test');
+        $program->setSlug($this->stringService->getSlug($program->getName()));
+        $program->setShortDescription('Short description');
+        $program->setIsSpotlighted(false);
+        $manager->persist($program);
+
+        // ProjectReference
+        $projectReference = new ProjectReference();
+        $projectReference->setName('Project Reference test');
+        $projectReference->setSlug($this->stringService->getSlug($projectReference->getName()));
+        $manager->persist($projectReference);
+
+        // CategoryTheme
+        $categoryTheme = new CategoryTheme();
+        $categoryTheme->setName('Category Theme test');
+        $categoryTheme->setSlug($this->stringService->getSlug($categoryTheme->getName()));
+        $categoryTheme->setShortDescription('Short description');
+        $manager->persist($categoryTheme);
+
+        // Category
+        $category = new Category();
+        $category->setName('Category test');
+        $category->setSlug($this->stringService->getSlug($category->getName()));
+        $category->setCategoryTheme($categoryTheme);
+        $category->setShortDescription('Short description');
+        $manager->persist($category);
+
+        // AidType
+        $aidType = new AidType();
+        $aidType->setName('Aide Type test');
+        $aidType->setSlug($this->stringService->getSlug($aidType->getName()));
+        $manager->persist($aidType);
+
+        // AidStep
+        $aidStep = new AidStep();
+        $aidStep->setName('Aide Step test');
+        $aidStep->setSlug($this->stringService->getSlug($aidStep->getName()));
+        $manager->persist($aidStep);
+
+        // Aid Recurrence
+        $aidRecurrence = new AidRecurrence();
+        $aidRecurrence->setName('Aide Recurrence test');
+        $aidRecurrence->setSlug($this->stringService->getSlug($aidRecurrence->getName()));
+        $manager->persist($aidRecurrence);
+
+        // AidDestination
+        $aidDestination = new AidDestination();
+        $aidDestination->setName('Aide Destination test');
+        $aidDestination->setSlug($this->stringService->getSlug($aidDestination->getName()));
+        $manager->persist($aidDestination);
+
+
+        // Aide
+        $aid = new Aid();
+        $aid->setPerimeter($perimeter);
+        $aid->setOrganization($organization);
+        $aid->setAuthor($admin);
+        $aid->setName('Aide test');
+        $aid->setNameInitial('Aide test');
+        $aid->setSlug($this->stringService->getSlug($aid->getName()));
+        $aid->addAidAudience($organizationType);
+
+        $aidFinancer = new AidFinancer();
+        $aidFinancer->setBacker($backer);
+        $aid->addAidFinancer($aidFinancer);
+
+        $aid->addAidType($aidType);
+        $aid->addAidStep($aidStep);
+        $aid->setAidRecurrence($aidRecurrence);
+        $aid->addAidDestination($aidDestination);
+
+        $aid->setDescription('Description longue');
+        $aid->setProjectExamples('Exemples de projets');
+        $aid->setContact('Contact');
+        $aid->addCategory($category);
+        $aid->addProjectReference($projectReference);
+        $aid->setEligibility('Eligibility');
+        $aid->setOriginUrl('https://www.example.com');
+        $aid->setApplicationUrl('https://www.example.com');
+
+        $aid->setStatus(Aid::STATUS_PUBLISHED);
+        $aid->setTimePublished(new \DateTime());
+        $aid->setDatePublished(new \DateTime());
+        $manager->persist($aid);
+
+
 
         /**
          * USER
