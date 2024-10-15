24bd99728afc24c148f4175954b03db129e68fce
Remi BARRET <barret.remi.pro@gmail.com>
2024-10-15 10:17:43 +0200

    nettoyage javascript

--- assets/js/admin/statistics/dashboard.js
+++ assets/js/admin/statistics/dashboard.js
@@ -1,18 +1,8 @@
 require('datatables');
 
 $(function(){
-
-    if ($('#table-top-aids').length) {
-        $('#table-top-aids').DataTable({
-            info: false,
-            "order": [[1, 'desc']],
-            "language": datatables_fr_strings,
-        });
-    }
-
     $('.dataTable').DataTable({
         info: false,
         'language': datatables_fr_strings,
     });
-
 });
