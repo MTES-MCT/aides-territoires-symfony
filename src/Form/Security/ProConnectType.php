888b9cd2ae334613a7ff2c59d52557c554b523c7
Remi BARRET <barret.remi.pro@gmail.com>
2024-11-05 09:28:08 +0100

    ajoute retour form proconnect

--- src/Form/Security/ProConnectType.php
+++ src/Form/Security/ProConnectType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class ProConnectType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
         ;
