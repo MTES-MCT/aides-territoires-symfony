06f2a5d68f5f8a2469d510f3833f1ac890437fb5
Remi BARRET <barret.remi.pro@gmail.com>
2024-11-15 16:01:47 +0100

    flux pays de la loire

--- src/Command/ImportFlux/ImportFluxCommand.php
+++ src/Command/ImportFlux/ImportFluxCommand.php
@@ -5,7 +5,10 @@ namespace App\Command\ImportFlux;
 use App\Entity\Aid\Aid;
 use App\Entity\Aid\AidFinancer;
 use App\Entity\Aid\AidRecurrence;
+use App\Entity\Aid\AidType;
+use App\Entity\Category\Category;
 use App\Entity\DataSource\DataSource;
+use App\Entity\Organization\OrganizationType;
 use App\Service\Email\EmailService;
 use App\Service\File\FileService;
 use App\Service\Perimeter\PerimeterService;
@@ -44,6 +47,12 @@ class ImportFluxCommand extends Command // NOSONAR too much methods
     protected ?AidRecurrence $aidRecurrenceOnGoing = null;
     protected ?AidRecurrence $aidRecurrenceRecurring = null;
 
+    /** @var array<int, AidType> */
+    protected array $aidTypesById = [];
+    /** @var array<string, array<int, Category>> */
+    protected array $aidCategoriesMapping = [];
+    /** @var array<int, OrganizationType> */
+    protected array $organizationTypesById = [];
     protected bool $paginationEnabled = false;
     protected int $nbPages = 1;
     protected int $nbByPages = 20;
@@ -80,6 +89,9 @@ class ImportFluxCommand extends Command // NOSONAR too much methods
         parent::__construct();
         $this->dateImportStart = new \DateTime(date('Y-m-d H:i:s'));
         $this->setInternalAidRecurrences();
+        $this->setAidTypesById();
+        $this->setOrganizationTypesById();
+        $this->setAidCategoriesMapping();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -251,6 +263,7 @@ class ImportFluxCommand extends Command // NOSONAR too much methods
                     $importUrl,
                     $this->getApiOptions()
                 );
+
                 $content = $response->getContent();
                 $content = $response->toArray();
 
@@ -746,8 +759,24 @@ class ImportFluxCommand extends Command // NOSONAR too much methods
         return '' !== $html ? $html : null;
     }
 
-    protected function setInternalAidRecurrences(): void // NOSONAR methode generique pour surcharge
+    protected function setInternalAidRecurrences(): void
     {
+        // méthode générique pour surcharge
+    }
+
+    protected function setAidTypesById(): void
+    {
+        // méthode générique pour surcharge
+    }
+
+    protected function setOrganizationTypesById(): void
+    {
+        // méthode générique pour surcharge
+    }
+
+    protected function setAidCategoriesMapping(): void
+    {
+        // méthode générique pour surcharge
     }
 
     protected function getValidExternalUrlOrNull(?string $url): ?string
