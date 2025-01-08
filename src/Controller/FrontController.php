912ee193fb83887ecb69a06852e97a168471f147
Remi BARRET <barret.remi.pro@gmail.com>
2025-01-08 11:06:03 +0100

    ajout de tests

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\Aid\AidRepository;
 use App\Service\Various\Breadcrumb;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,5 +33,47 @@ class FrontController extends AbstractController
             $type,
             $this->translatorInterface->trans($message)
         );
+    }
+
+    #[Route('/test_request/', name: 'app_test_request')]
+    public function testRequests(
+        AidRepository $aidRepository,
+        ManagerRegistry $managerRegistry
+    ): Response
+    {
+        $response = '';
+
+        $timeStart = microtime(true);
+        $countAids = $aidRepository->countCustom();
+        unset($countAids);
+        $managerRegistry->getManager()->clear();
+        $timeEnd = microtime(true);
+        $executionTime = $timeEnd - $timeStart;
+        $response .= '<p>'.(round($executionTime * 1000)).'</p>';
+
+        $timeStart = microtime(true);
+        $aids = $aidRepository->findCustom([
+            'showInSearch',
+            'maxResults' => 1000
+        ]);
+        unset($aids);
+        $managerRegistry->getManager()->clear();
+        $timeEnd = microtime(true);
+        $executionTime = $timeEnd - $timeStart;
+        $response .= '<p>'.(round($executionTime * 1000)).'</p>';
+
+        $timeStart = microtime(true);
+        $qb = $aidRepository->getQueryBuilderForSearch([
+            'showInSearch' => true
+        ]);
+        $aids = $qb->getQuery()->getResult();
+        unset($aids);
+        $managerRegistry->getManager()->clear();
+        $timeEnd = microtime(true);
+        $executionTime = $timeEnd - $timeStart;
+        $response .= '<p>'.(round($executionTime * 1000)).'</p>';
+
+        
+        return new Response($response);
     }
 }
